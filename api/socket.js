93ac4f208c20193da197251211c38a56488339a2
Kevin Gage <dudeman02379@gmail.com>
2020-11-04 13:10:40 -0500

    added sessions to socketio

--- api/socket.js
+++ api/socket.js
@@ -1,10 +1,25 @@
 const socket_io = require('socket.io');
 const io = socket_io();
 const socketApi = {};
+const session = require('express-session');
+
+// Initialize sessions for sockets
+const sessionMiddleware = session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }});
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+  // sessionMiddleware(socket.request, socket.request.res, next); will not work with websocket-only
+  // connections, as 'socket.request.res' will be undefined in that case
+});
 
 io.on('connection', socket => {
+  // get session and add a property to it
+  const session = socket.request.session;
+  session.messageCount = 0;
+  session.save();
+
   console.log('a user connected');
-  
+
   socket.on('disconnect', reason => {
     console.log('user disconnected');
   });
--- api/sockets/messages.js
+++ api/sockets/messages.js
@@ -1,6 +1,13 @@
 module.exports = function(socket) {
   socket.on('new message', data => {
+    //get message count from session and incriment it
+    const session = socket.request.session;
+    session.messageCount++;
+    session.save();
     console.log(`new message: ${data.room}`);
+    console.log(session.messageCount);
+
+    //Broadcast something
     socket.broadcast
     .to(data.room)
     .emit('receive message', data)
